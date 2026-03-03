72f264745bab93636defbe3964ff8d5374dacacb
Dilukshan <65407969+dilukshann7@users.noreply.github.com>
2026-03-03 23:13:09 +0530

    feat: Configure main Electron window and load HTML content

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,24 @@
 import { app, BrowserWindow } from "electron";
+import path from "path";
+
+process.env.NODE_ENV = process.env.NODE_ENV || "development";
+
+const isDev = process.env.NODE_ENV === "development";
+const isMac = process.platform === "darwin";
 
 function createMainWindow(): void {
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+    resizable: isDev,
+    icon: path.join(__dirname, "../assets/icon.png"),
   });
+
+  mainWindow.loadFile(path.join(__dirname, "../src/index.html"));
 }
 
 app.on("ready", createMainWindow);
