aa618e3e2ef951e0d76d724924efbf233f172076
Dilukshan <65407969+dilukshann7@users.noreply.github.com>
2026-03-04 02:04:55 +0530

    feat: Initialize React for renderer processes

---
+++ src/renderer/components/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App: React.FC = () => {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen bg-[#222222] text-[#f0f0f0] font-sans">
+      <h1 className="text-4xl font-bold">Notesploy</h1>
+    </div>
+  );
+};
+
+export default App;
