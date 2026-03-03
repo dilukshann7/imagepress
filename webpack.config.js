d16c60c4cd5bc754cd880df672ad72e092693883
Dilukshan <65407969+dilukshann7@users.noreply.github.com>
2026-03-04 02:04:56 +0530

    build: Add Webpack configuration for React and CSS

---
+++ webpack.config.js
@@ -0,0 +1,61 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = [
+  {
+    name: "renderer",
+    entry: "./src/renderer/index.tsx",
+    target: "electron-renderer",
+    mode: process.env.NODE_ENV === "production" ? "production" : "development",
+    devtool: "source-map",
+    plugins: [new MiniCssExtractPlugin({ filename: "renderer.css" })],
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: "ts-loader",
+          exclude: /node_modules/,
+        },
+        {
+          test: /\.css$/,
+          use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader"],
+        },
+      ],
+    },
+    resolve: {
+      extensions: [".tsx", ".ts", ".js"],
+    },
+    output: {
+      filename: "renderer.js",
+      path: path.resolve(__dirname, "dist"),
+    },
+  },
+  {
+    name: "about",
+    entry: "./src/renderer/about.tsx",
+    target: "electron-renderer",
+    mode: process.env.NODE_ENV === "production" ? "production" : "development",
+    devtool: "source-map",
+    plugins: [new MiniCssExtractPlugin({ filename: "about.css" })],
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: "ts-loader",
+          exclude: /node_modules/,
+        },
+        {
+          test: /\.css$/,
+          use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader"],
+        },
+      ],
+    },
+    resolve: {
+      extensions: [".tsx", ".ts", ".js"],
+    },
+    output: {
+      filename: "about.js",
+      path: path.resolve(__dirname, "dist"),
+    },
+  },
+];
